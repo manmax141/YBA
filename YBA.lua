a92c3a361e2c40a748f8a736971619f5e55311ad
manmax141 <68015857+manmax141@users.noreply.github.com>
2021-08-25 13:30:03 +0200

    Update YBA.lua

--- YBA.lua
+++ YBA.lua
@@ -297,8 +297,8 @@ local AutoAttachToggle = MiscChannel:Toggle("Auto Attach", false, function(bool)
         local stand = char:WaitForChild("StandMorph")
         local standhrp = stand:WaitForChild("HumanoidRootPart")
 
-        standhrp.StandAttach.AlignPosition.Attachment1 = getgenv().standAttachTarget:WaitForChild("Head"):WaitForChild("FaceFrontAttachment")
-        standhrp.StandAttach.AlignOrientation.Attachment1 = getgenv().standAttachTarget:WaitForChild("Head"):WaitForChild("FaceFrontAttachment")
+        standhrp.StandAttach.AlignPosition.Attachment1 = getgenv().standAttachTarget:WaitForChild("LowerTorso"):WaitForChild("WaistCenterAttachment")
+        standhrp.StandAttach.AlignOrientation.Attachment1 = getgenv().standAttachTarget:WaitForChild("LowerTorso"):WaitForChild("WaistCenterAttachment")
       else
         local stand = char:WaitForChild("StandMorph")
         local standhrp = stand:WaitForChild("HumanoidRootPart")
